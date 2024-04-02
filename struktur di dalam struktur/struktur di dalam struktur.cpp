b4f3e9aee2c8b9659805008f5cd848b083aff9b7
naxsyambis <naxsyambis@gmail.com>
2024-04-02 12:55:21 +0100

    mendisplay tabel tabel

--- struktur di dalam struktur/struktur di dalam struktur.cpp
+++ struktur di dalam struktur/struktur di dalam struktur.cpp
@@ -11,7 +11,7 @@ struct Mahasiswa {
 	string nama;
 	DetailAlamat alamat;
 	int umur;
-};
+}; 
 
 int main()
 {
@@ -30,8 +30,13 @@ int main()
 	cout << " Umur Mahasiswa : ";
 	cin >> mhs.umur;
 
-
-
+	cout << endl;
+	cout << "\n NIM : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : ";
+	cout << "\n \t Desa : " << mhs.alamat.desa;
+	cout << "\n \t Kota : " << mhs.alamat.kota;
+	cout << "\n Umur :" << mhs.umur;
 
 }
 
