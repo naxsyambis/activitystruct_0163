da0beaaa98a1b01bdefe0586aa3a7e515a6c690a
naxsyambis <naxsyambis@gmail.com>
2024-04-02 14:23:30 +0100

    display data stuktur

--- kombinasi dengan Array/kombinasi dengan Array.cpp
+++ kombinasi dengan Array/kombinasi dengan Array.cpp
@@ -36,6 +36,19 @@ int main()
 		cin.ignore(1, '\n');
 		cout << endl;
 	}
+
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "Data Mahasiswa ke- " << (i + 1) << ": ";
+		cout << "\n NIM : " << mhs[i].nim;
+		cout << "\n Nama : " << mhs[i].nama;
+		cout << "\n Alamat : ";
+		cout << "\n \t Desa : " << mhs[i].alamat.desa;
+		cout << "\n \t Kota : " << mhs[i].alamat.kota;
+		cout << "\n Umur : " << mhs[i].umur;
+		cout << endl;
+	}
+
 }
 
 
