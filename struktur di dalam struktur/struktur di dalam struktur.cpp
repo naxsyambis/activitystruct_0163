04d50ee4f044c100e021cc29690e9eb8f391572e
naxsyambis <naxsyambis@gmail.com>
2024-04-02 12:47:15 +0100

    mengimput alamat mahasiswa, nama desa ke mhs.alamat.desa; nama kota ke mhs.alamat.kota

--- struktur di dalam struktur/struktur di dalam struktur.cpp
+++ struktur di dalam struktur/struktur di dalam struktur.cpp
@@ -21,6 +21,13 @@ int main()
 	cout << "Nama Mahasiswa : ";
 	cin >> mhs.nama;
 
+	cout << "Alamat Mahasiswa : " << endl;
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama Kota : ";
+	cin >> mhs.alamat.kota;
+
+
 
 
 }
